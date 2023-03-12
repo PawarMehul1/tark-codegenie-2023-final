9fa352caa8b0b67148d25954a4071e56e0ab331f
PawarMehul1 <111976321+PawarMehul1@users.noreply.github.com>
2023-03-12 15:09:44 +0530

    final submition

--- Book_Tickets.cpp
+++ Book_Tickets.cpp
@@ -20,21 +20,46 @@ class Seats
         slots=all;
     }
 
+    int getsize()
+    {
+        return slots.size();
+    }
+    string getName()
+    {
+        return name;
+    }
+
+};
+
+class detail
+{
+    public:
+    long long pnr;
+    vector<string> sits;
+
+    detail(long long p,vector<string> sit)
+    {
+        this->pnr=pnr;
+        this->sits=sit;
+    }
 };
 
 class Train
 {
     public:
     int No;
+    int distanc;
     string station1;
     string station2;
     vector<Seats> all;
 
-    Train(int no, string s1,string s2)
+    Train(int no, string s1,string s2,int d)
     {
+
         this->No=no;
         this->station1=s1;
         this->station2=s2;
+        this->distanc=d;
     }
 
     int getNo()
@@ -55,8 +80,19 @@ class Train
     void addseats(string type,int size)
     {
         Seats s(type,size);
+        this->all.push_back(s);
+    }
 
-        all.push_back(s);
+    void display()
+    {
+        for(int i=0; i<all.size(); i++)
+        {
+            cout<<all[i].getName()<<" "<<all[i].getsize()<<endl;
+        }    
+    }
+    vector<Seats> giveset()
+    {
+        return this->all;
     }
 
 };
@@ -114,18 +150,125 @@ int strtoint(string s)
 void allocate(string str,vector<Train>& alltrain)
 {
     vector<string> tocken=split(str);
-    int no=strtoint(tocken[0]);
+    int n=alltrain.size();
 
-    for(int i=alltrain.size()-1; i>=0; i--)
+            for(int j=1; j<tocken.size(); j++)
+            {
+                string tmp=tocken[j];
+                int index=0;
+                int len=tmp.length();
+
+                for(; index<len; index++)
+                {
+                    if(tmp[index]=='-')
+                        break;
+                }
+
+                string s1=tmp.substr(0,index);
+                string s2=tmp.substr(index+1,len-index-1);
+
+                int size=strtoint(s2);
+
+                alltrain[n-1].addseats(s1,size);
+    
+            }
+
+}
+vector<detail> book(string details,vector<Train>& alltrain,vector<detail>& info)
+{
+    // Input: Rajkot Mumbai 2023-03-15 SL 6
+    vector<string> tocken=split(details);
+    int n=strtoint(tocken[4]);
+
+    if(tocken[3]=="SL")
+        tocken[3]="S";
+    else if(tocken[3]=="3A")
+        tocken[3]="B";
+    else if(tocken[3]=="2A")
+        tocken[3]="A";
+    else
+        tocken[3]="H";   
+
+    int count=0; 
+    int price=n;       
+    vector<string> seats; 
+    for(int i=0; i<alltrain.size(); i++)
     {
-        cout<<alltrain[i].getNo()<<endl;
+        if((count<n)&&(tocken[0]==alltrain[i].station1 && tocken[1]==alltrain[i].station2)||(tocken[0]==alltrain[i].station2 && tocken[1]==alltrain[i].station1))
+        {
+            price*=alltrain[i].distanc;
+            //alltrain[i]
+            for(int j=0; j<alltrain[i].all.size() && count<n; j++)
+            {
+                if(tocken[3][0]==alltrain[i].all[j].name[0])
+                {
+                    for(int k=0; k<alltrain[i].all[j].slots.size() && (count<n); k++)
+                    {
+                        if(alltrain[i].all[j].slots[k].size()==0)
+                        {
+                            alltrain[i].all[j].slots[k].push_back(tocken[2]);
+                            count++;
+                            string sit=tocken[3];
+                            sit+='-';
+                            sit+=inttostr(k+1);
+                            seats.push_back(sit);
+                        }
+                        else
+                        {
+                            bool flag=true;
+                            for(int m=0; m<alltrain[i].all[j].slots[k].size(); m++)
+                            {
+                                if(alltrain[i].all[j].slots[k][m]==tocken[2])
+                                    flag=false;
+
+                            }
+
+                            if(flag)
+                            {
+                                alltrain[i].all[j].slots[k].push_back(tocken[2]);
+                            count++;
+                            string sit=tocken[3];
+                            sit+='-';
+                            sit+=inttostr(k+1);
+                            seats.push_back(sit);
+                            }
+
+
+                        }
+                    }
+                }
+            }
+            break;
+        }
+    }
+    if(count==n)
+    {
+        if(tocken[3]=="B")
+            price*=2;
+        else if(tocken[3]=="A")
+            price*=3;
+        else if(tocken[3]=="H")  
+            price*=4;      
+        
+        cout<<PNR<<" "<<price<<endl;
+        PNR++;
+        detail d(PNR,seats);
+        info.push_back(d);
+
+    }
+    else
+    {
+        cout<<"NO SEATS ARE VAILABLE"<<endl;
     }
 
+
+
+    return info;
 }
 int main()
 {
 
-    // 2
+    // 1
     // 17726 Rajkot-0 Mumbai-750
     // 17226 S1-72 S2-72 B1-72 A1-48 H1-24
     // 37392 Ahmedabad-0 Surat-300
@@ -148,14 +291,43 @@ int main()
     for(int i=1; i<=(2*n); i+=2)
     {
         vector<string> tocken=split(details[i]);
-        Train T(strtoint(tocken[0]),tocken[1],tocken[2]);
+
+        string s1=tocken[1];
+        int index=0;
+
+        for(; index<s1.length(); index++)
+            if(s1[index]=='-')
+                break;
+
+        string tmp1=s1.substr(0,index);
+        string tmp2=s1.substr(index+1,s1.length()-index-1);
+
+        
+        string s2=tocken[2];
+
+        for(index=0; index<s2.length(); index++)
+            if(s1[index]=='-')
+                break;
+
+        string tmp3=s2.substr(0,index);
+        string tmp4=s2.substr(index+1,s2.length()-index-1);
+
+
+        Train T(strtoint(tocken[0]),tmp1,tmp3,strtoint(tmp4)-strtoint(tmp2));
 
         alltrain.push_back(T);
 
         allocate(details[i+1],alltrain);
-        
     }
 
-    
+    vector<detail> info;
+    while(true)
+    {
+        getline(cin,input);
+        if(input=="EXIT")
+            break;
+
+        info=book(input,alltrain,info);
+    }
     return 0;
 }
